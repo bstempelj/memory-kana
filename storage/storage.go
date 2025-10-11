01b2e9de5af1b489342bcc8c8169eb8d95fb2f78
Blaž Štempelj <blaz.stempelj@proton.me>
2025-10-11 20:25:49 +0200

    wip: convert time column to bigint

--- storage/storage.go
+++ storage/storage.go
@@ -13,9 +13,9 @@ const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-type PlayerTime struct {
-	Player string
-	Time   time.Time
+type PlayerDuration struct {
+	Player   string
+	Duration time.Duration
 }
 
 type Config struct {
@@ -43,7 +43,7 @@ func Connect(cfg Config) (*sql.DB, error) {
 	return db, nil
 }
 
-func InsertPlayerTime(db *sql.DB, playerTime time.Time) (string, error) {
+func InsertPlayerDuration(db *sql.DB, duration time.Duration) (string, error) {
 	randHash := func(length int) string {
 		b := make([]byte, length)
 		for i := range b {
@@ -52,59 +52,69 @@ func InsertPlayerTime(db *sql.DB, playerTime time.Time) (string, error) {
 		return string(b)
 	}
 
-	playerName := "guest-" + randHash(8)
+	player := "guest-" + randHash(8)
 
 	_, err := db.Exec(
-		"insert into player_times(player, time) values ($1, $2)",
-		playerName,
-		playerTime)
+		`insert into player_times(player, duration) values ($1, $2)`,
+		player, duration.Nanoseconds())
 	if err != nil {
 		return "", err
 	}
-	return playerName, nil
+	return player, nil
 }
 
-func SelectPlayerTimes(db *sql.DB) ([]PlayerTime, error) {
-	var playerTimes []PlayerTime
+func SelectPlayerDurationList(db *sql.DB) ([]PlayerDuration, error) {
+	var playerDurationList []PlayerDuration
 
-	res, err := db.Query(`select player, "time" from player_times order by "time" limit 10`)
+	res, err := db.Query(`
+		select player, duration
+		from player_times
+		order by duration
+		limit 10
+	`)
 	if err != nil {
 		return nil, err
 	}
 	defer res.Close()
 
 	for res.Next() {
-		var playerTime PlayerTime
+		var playerDuration PlayerDuration
 
-		err := res.Scan(&playerTime.Player, &playerTime.Time)
+		err := res.Scan(&playerDuration.Player, &playerDuration.Duration)
 		if err != nil {
 			return nil, err
 		}
 
-		playerTimes = append(playerTimes, playerTime)
+		playerDurationList = append(playerDurationList, playerDuration)
 	}
 
 	if err = res.Err(); err != nil {
 		return nil, err
 	}
-	return playerTimes, nil
+	return playerDurationList, nil
 }
 
-func SelectPlayerTimeRank(db *sql.DB, player string) (time.Time, uint, error) {
-	var playerTime time.Time
-	var playerRank uint
+func SelectPlayerDurationAndRank(db *sql.DB, player string) (time.Duration, uint, error) {
+	var (
+		duration time.Duration
+		rank     uint
+	)
 
-	row := db.QueryRow(`select "time" from player_times where player = $1`, player)
-	err := row.Scan(&playerTime)
+	row := db.QueryRow(`
+		select duration
+		from player_times
+		where player = $1
+	`, player)
+	err := row.Scan(&duration)
 	if err != nil && errors.Is(err, sql.ErrNoRows) {
-		return time.Time{}, 0, errors.New("no rows returned when querying for player time")
+		return time.Duration(0), 0, errors.New("no rows returned when querying for player duration")
 	}
 
-	row = db.QueryRow(`select count(1) from player_times where "time" <= $1`, playerTime)
-	err = row.Scan(&playerRank)
+	row = db.QueryRow(`select count(1) from player_times where duration <= $1`, duration)
+	err = row.Scan(&rank)
 	if err != nil && errors.Is(err, sql.ErrNoRows) {
-		return time.Time{}, 0, errors.New("no rows returned when querying for player rank")
+		return time.Duration(0), 0, errors.New("no rows returned when querying for player rank")
 	}
 
-	return playerTime, playerRank, nil
+	return duration, rank, nil
 }
